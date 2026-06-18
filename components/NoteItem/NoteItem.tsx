c230f4da88ead4a3a48bb0537f9bd8111a645429
Mykyta <burenoknikitad@gmail.comgit config --global user.name Mykytagit config --global user.email burenoknikitad@gmail.com>
2026-06-18 12:01:23 +0200

    Fix direction

--- components/NoteItem/NoteItem.tsx
+++ components/NoteItem/NoteItem.tsx
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import css from "./NoteItem.module.css";
 import type { Note } from "../../types/note";
-import { deleteNote } from "../../lib/api";
+import { deleteNote } from "../../lib/api/clientApi";
 
 interface NoteListProps {
   notes: Note[];
